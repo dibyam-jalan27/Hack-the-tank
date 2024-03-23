a7fb86f8244f00287d9680330a8f6a0abd03d2ec
Story <pathakharshit281@gmail.com>
2024-03-23 16:06:42 +0530

    card component made

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Card1 from './UI/Card1';
 
 function App() {
   return (
     <>
-       
+       <Card1 
+        image="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+        title="Shoes!"
+        body="If a dog chews shoes whose shoes does he choose?"
+        primaryBadges={['NEW']}
+        secondaryBadges={['Fashion', 'Products']}
+        price="100"
+      />
     </>
   );
 }
