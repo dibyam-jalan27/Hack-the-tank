42c89fda67abd7de5a17e03e2cdfce6f2e2f8fd4
Krupal Chauhdari <u21ec146@eced.svnit.ac.in>
2024-03-23 15:33:11 +0530

    review

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import Forgot from './components/Forgot';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Signup from './components/Signup';
 import ResetPassword from './components/ResetPassword';
-
+import Review from './components/Review';
+import CourseDetails from './components/CourseDetails';
 function App() {
   return (
 <BrowserRouter>
@@ -14,6 +15,7 @@ function App() {
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/reset-password" element={<ResetPassword/>}/>
       <Route path='/review' element={<Review/>}></Route>
+      <Route path='/course-details' element={<CourseDetails/>}></Route>
     </Routes>
 </BrowserRouter>
   );
