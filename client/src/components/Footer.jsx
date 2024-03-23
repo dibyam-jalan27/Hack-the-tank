3752e7ac05ec55a48e11f824967c2304c9831152
Krupal Chauhdari <u21ec146@eced.svnit.ac.in>
2024-03-23 21:19:27 +0530

    pubchbsh

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,42 +6,14 @@ const Footer = () => {
     <footer className="footer-section">
       <div className="container">
         <div className="footer-cta pt-5 pb-5">
-          <div className="row">
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="fas fa-map-marker-alt"></i>
-                <div className="cta-text">
-                  <h4>Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="fas fa-phone"></i>
-                <div className="cta-text">
-                  <h4>Call us</h4>
-                  <span>9876543210 0</span>
-                </div>
-              </div>
-            </div>
-            <div className="col-xl-4 col-md-4 mb-30">
-              <div className="single-cta">
-                <i className="far fa-envelope-open"></i>
-                <div className="cta-text">
-                  <h4>Mail us</h4>
-                  <span>mail@info.com</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
         <div className="footer-content pt-5 pb-5">
           <div className="row">
             <div className="col-xl-4 col-lg-4 mb-50">
               <div className="footer-widget">
                 <div className="footer-logo">
-                  <a href="index.html"><img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo" /></a>
+                  <a href="index.html"><img src='./rajaranilogo.webp' className="img-fluid" alt="logo" /></a>
                 </div>
                 <div className="footer-text">
                   <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing elit,Lorem ipsum dolor sit amet.</p>
@@ -49,7 +21,7 @@ const Footer = () => {
                 <div className="footer-social-icon">
                   <span>Follow us</span>
                   <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
-                  <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
+                  <a href="#"><i className="fab fa-instagram twitter-bg"></i></a>
                   <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
                 </div>
               </div>
@@ -94,23 +66,11 @@ const Footer = () => {
       </div>
       <div className="copyright-area">
         <div className="container">
-          <div className="row">
-            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+          <div className="row ">
               <div className="copyright-text">
-                <p>Copyright &copy; 2018, All Right Reserved <a href="https://codepen.io/anupkumar92/">Anup</a></p>
+                <p>Copyright &copy; 2024, All Right Reserved <a href="https://codepen.io/anupkumar92/">Strikers</a></p>
               </div>
-            </div>
-            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
-              <div className="footer-menu">
-                <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">Terms</a></li>
-                  <li><a href="#">Privacy</a></li>
-                  <li><a href="#">Policy</a></li>
-                  <li><a href="#">Contact</a></li>
-                </ul>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
